1c5f36dc8d29fb785a45642b01ef0b23577a585e
Win-10 <yadavashutosh2193@gmail.com>
2020-12-02 10:20:42 +0530

    apply color done

--- src/components/App.js
+++ src/components/App.js
@@ -26,7 +26,8 @@ const title = 'Select the gradient and then the Box to change the color';
 const App = () => {
   let [nextBackground, selectNextBackground] = useState({ background: "" })
   const applyColor = (updateSelectionStyle) => {
-    updateSelectionStyle(nextBackground)
+    // updateSelectionStyle(nextBackground)
+    selectNextBackground(updateSelectionStyle);
   }
 
   return (
--- src/components/Selection.js
+++ src/components/Selection.js
@@ -2,9 +2,9 @@
 import React, { useState } from 'react';
 
 function Selection(props){
-    const [Backgroundcolor, setbackgroundColor] = useState({background:''});
+    // const [Backgroundcolor, setbackgroundColor] = useState({background:''});
  return (<>
-     <div className = "fix-box" style = {{background: Backgroundcolor}} onClick={()=>setbackgroundColor(props.applyColor)}>
+     <div className = "fix-box" style = {{background: props.backgc}} onClick={props.applyColor(props.backgc)}>
             <h2 className = "subheading">{props.selectionName}</h2>
      </div>
  </>);
