fc389d041070f8e8a8b85bbf56f211127554c545
Win-10 <yadavashutosh2193@gmail.com>
2020-12-26 21:59:46 +0530

    applyColor Colled

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -4,8 +4,12 @@ import React, { useState } from 'react';
 function Selection(props){
     const [Backgroundcolor, setbackgroundColor] = useState({background:''});
     // console.log(Backgroundcolor);
+    const DivClick = ()=>{
+        props.applyColor("green");
+        setbackgroundColor({background: props.backgc})
+    }
  return (<>
-     <div className = "fix-box" style = {{background: Backgroundcolor.background}} onClick={()=>setbackgroundColor({background: props.backgc})}>
+     <div className = "fix-box" style = {{background: Backgroundcolor.background}} onClick={DivClick}>
             <h2 className = "subheading">{props.selectionName}</h2>
      </div>
  </>);
